364270e23ad0ce284413d2152cbaa1ab40ce873d
munenebrian <njerubrianmunene@gmail.com>
2022-08-21 22:07:35 +0300

    done!

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,28 @@
 require 'pry'
 
 class String
+  attr_accessor :string
 
   def sentence?
-    
+    self.end_with?(".")
+
   end
 
   def question?
+    self.end_with?('?')
 
   end
 
   def exclamation?
+    self.end_with?('!')
 
   end
 
   def count_sentences
+    self.split(/(?<=[.!?]).(?=[a-zA-Z])/).count
 
   end
 end
+mandem = String.new('Uweeh, wacha tuh!!')
+puts mandem.sentence?
+puts mandem.count_sentences
